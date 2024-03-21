54882b42e28998f3a2bd7bc4a9dab3995929b4bf
Stephen Moore <stephen@delfick.com>
2024-03-22 08:53:35 +1100

    Small reshuffle

--- djangomypytest/mypy_plugin/plugin.py
+++ djangomypytest/mypy_plugin/plugin.py
@@ -4,7 +4,7 @@ from functools import partial
 from typing import TypedDict, cast
 
 from mypy.nodes import TypeInfo
-from mypy.plugin import AnalyzeTypeContext, ClassDefContext
+from mypy.plugin import AnalyzeTypeContext, ClassDefContext, SymbolTableNode
 from mypy.semanal import SemanticAnalyzer
 from mypy.typeanal import TypeAnalyser
 from mypy.types import Instance, ProperType, UnionType, get_proper_type
@@ -32,7 +32,11 @@ class ExtendedMypyStubs(main.NewSemanalDjangoPlugin):
         self, fullname: str
     ) -> Callable[[AnalyzeTypeContext], MypyType] | None:
         if fullname == "djangomypytest.mypy_plugin.annotations.Concrete":
-            return partial(find_concrete_models, django_context=self.django_context, plugin=self)
+            return partial(
+                find_concrete_models,
+                django_context=self.django_context,
+                concrete_children=self.concrete_children,
+            )
         else:
             return super().get_type_analyze_hook(fullname)
 
@@ -40,35 +44,45 @@ class ExtendedMypyStubs(main.NewSemanalDjangoPlugin):
         self, fullname: str
     ) -> Callable[[ClassDefContext], None] | None:
         if fullname:
-            sym = self.lookup_fully_qualified(fullname)
-            if sym is not None and isinstance(sym.node, TypeInfo) and len(sym.node.mro) > 2:
-                if any(
-                    m.fullname == fullnames.MODEL_CLASS_FULLNAME for m in sym.node.mro
-                ) and not helpers.is_abstract_model(sym.node):
-                    for typ in sym.node.mro[1:-2]:
-                        if typ.fullname != sym.node.fullname and helpers.is_abstract_model(typ):
-                            meta = get_extended_django_metadata(typ)
-                            if "concrete_children" not in meta:
-                                meta["concrete_children"] = self.concrete_children[typ.fullname]
-
-                            if sym.node.fullname not in meta["concrete_children"]:
-                                meta["concrete_children"].append(sym.node.fullname)
-
-                            reviewed: list[str] = []
-                            for child in meta["concrete_children"]:
-                                child_sym = self.lookup_fully_qualified(child)
-                                if child_sym and isinstance(child_sym.node, TypeInfo):
-                                    if not helpers.is_abstract_model(child_sym.node):
-                                        reviewed.append(child_sym.node.fullname)
-
-                            if reviewed != meta["concrete_children"]:
-                                meta["concrete_children"].clear()
-                                meta["concrete_children"].extend(reviewed)
+            _fill_out_concrete_children(
+                fullname, self.concrete_children, self.lookup_fully_qualified
+            )
         return super().get_customize_class_mro_hook(fullname)
 
 
+def _fill_out_concrete_children(
+    fullname: str,
+    concrete_children: dict[str, list[str]],
+    lookup_fully_qualified: Callable[[str], SymbolTableNode | None],
+) -> None:
+    sym = lookup_fully_qualified(fullname)
+    if sym is not None and isinstance(sym.node, TypeInfo) and len(sym.node.mro) > 2:
+        if any(
+            m.fullname == fullnames.MODEL_CLASS_FULLNAME for m in sym.node.mro
+        ) and not helpers.is_abstract_model(sym.node):
+            for typ in sym.node.mro[1:-2]:
+                if typ.fullname != sym.node.fullname and helpers.is_abstract_model(typ):
+                    meta = get_extended_django_metadata(typ)
+                    if "concrete_children" not in meta:
+                        meta["concrete_children"] = concrete_children[typ.fullname]
+
+                    if sym.node.fullname not in meta["concrete_children"]:
+                        meta["concrete_children"].append(sym.node.fullname)
+
+                    reviewed: list[str] = []
+                    for child in meta["concrete_children"]:
+                        child_sym = lookup_fully_qualified(child)
+                        if child_sym and isinstance(child_sym.node, TypeInfo):
+                            if not helpers.is_abstract_model(child_sym.node):
+                                reviewed.append(child_sym.node.fullname)
+
+                    if reviewed != meta["concrete_children"]:
+                        meta["concrete_children"].clear()
+                        meta["concrete_children"].extend(reviewed)
+
+
 def find_concrete_models(
-    ctx: AnalyzeTypeContext, django_context: DjangoContext, plugin: ExtendedMypyStubs
+    ctx: AnalyzeTypeContext, django_context: DjangoContext, concrete_children: dict[str, list[str]]
 ) -> ProperType:
     args = ctx.type.args
     type_arg = ctx.api.analyze_type(args[0])
@@ -85,7 +99,7 @@ def find_concrete_models(
 
     meta = get_extended_django_metadata(type_arg.type)
     if "concrete_children" not in meta:
-        meta["concrete_children"] = plugin.concrete_children[type_arg.type.fullname]
+        meta["concrete_children"] = concrete_children[type_arg.type.fullname]
 
     concrete: list[Instance] = []
     reviewed: list[str] = []
