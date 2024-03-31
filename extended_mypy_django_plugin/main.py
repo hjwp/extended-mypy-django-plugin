17971adaf13ff13f6df50f2d9fbf2ba7284de036
Stephen Moore <stephen@delfick.com>
2024-03-31 14:26:18 +1100

    Make dmypy run less confusing
    Don't recreate the plugin in dmypy runs

--- extended_mypy_django_plugin/main.py
+++ extended_mypy_django_plugin/main.py
@@ -1,11 +1,24 @@
 from typing import cast
 
 from mypy.options import Options
+from mypy.plugin import Plugin as MypyPlugin
 
 from .plugin import ExtendedMypyStubs
 
+# This lets us work out if we're in dmypy
+# dmypy will recall "plugin" below which will
+# make a new plugin, but without necessarily replacing
+# existing plugin hooks, which is confusing
+created: bool = False
 
-def plugin(version: str) -> type[ExtendedMypyStubs]:
+
+def plugin(version: str) -> type[MypyPlugin]:
+    global created
+    if created:
+        # Inside dmypy, don't create a new plugin
+        return MypyPlugin
+
+    created = True
     major, minor, _ = version.split(".", 2)
 
     class Plugin(ExtendedMypyStubs):
