99c569f08e52d49b82c94a188b059bc572740b9d
Stephen Moore <stephen@delfick.com>
2024-05-07 14:26:01 +1000

    deps: upgrade pytest-mypy-plugins dep
    Using the branch where we can write tests with followups

--- scripts/tests_extension_hook.py
+++ scripts/tests_extension_hook.py
@@ -1,61 +1,153 @@
+import ast
 import os
 import pathlib
+import runpy
+from collections.abc import Mapping, MutableSequence
+from typing import TYPE_CHECKING
 
-from pytest_mypy_plugins.collect import File
-from pytest_mypy_plugins.item import YamlTestItem
+from pytest_mypy_plugins import (
+    ExtensionHook,
+    FollowupFile,
+    ItemForHook,
+    MypyPluginsScenario,
+    OutputMatcher,
+    ScenarioHookMaker,
+    ScenarioHooks,
+    ScenarioHooksRunAndCheckOptions,
+)
 
 here = pathlib.Path(__file__).parent
 
 
-def django_plugin_hook(test_item: YamlTestItem) -> None:
-    custom_settings = test_item.parsed_test_data.get("custom_settings", "")
-    installed_apps = test_item.parsed_test_data.get("installed_apps", ["myapp", "myapp2"])
-    monkeypatch = test_item.parsed_test_data.get("monkeypatch", False)
-
-    if installed_apps and custom_settings:
-        raise ValueError(
-            '"installed_apps" and "custom_settings" are not compatible, please use one or the other'
-        )
-
-    if installed_apps is not None:
-        # custom_settings is empty, add INSTALLED_APPS
-        if "django.contrib.contenttypes" not in installed_apps:
-            installed_apps += ["django.contrib.contenttypes"]
-        installed_apps_as_str = "(" + ",".join([repr(app) for app in installed_apps]) + ",)"
-        custom_settings += "INSTALLED_APPS = " + installed_apps_as_str
-
-    if "SECRET_KEY" not in custom_settings:
-        custom_settings = 'SECRET_KEY = "1"\n' + custom_settings
-
-    if monkeypatch:
-        custom_settings = (
-            "import django_stubs_ext\ndjango_stubs_ext.monkeypatch()\n" + custom_settings
-        )
-
+def django_plugin_hook(item: ItemForHook) -> None:
     django_settings_section = (
         "\n[mypy.plugins.django-stubs]\n" "django_settings_module = mysettings"
     )
-    if not test_item.additional_mypy_config:
-        test_item.additional_mypy_config = django_settings_section
+
+    if not item.additional_mypy_config:
+        item.additional_mypy_config = django_settings_section
     else:
-        if "[mypy.plugins.django-stubs]" not in test_item.additional_mypy_config:
-            test_item.additional_mypy_config += django_settings_section
+        if "[mypy.plugins.django-stubs]" not in item.additional_mypy_config:
+            item.additional_mypy_config += django_settings_section
 
-    mysettings_file = File(path="mysettings.py", content=custom_settings)
 
-    test_item.files.append(mysettings_file)
+class Hooks(ScenarioHooks):
+    def before_run_and_check_mypy(
+        self,
+        *,
+        scenario: MypyPluginsScenario,
+        options: ScenarioHooksRunAndCheckOptions,
+        config_file: pathlib.Path,
+        expected_output: MutableSequence[OutputMatcher],
+        additional_properties: Mapping[str, object],
+    ) -> ScenarioHooksRunAndCheckOptions:
+        custom_settings = additional_properties.get("custom_settings", None)
+        installed_apps = additional_properties.get("installed_apps", None)
+        monkeypatch = additional_properties.get("monkeypatch", None)
 
-    if "myapp" in installed_apps:
-        for root, _, files in os.walk(here / "myapp"):
-            for name in files:
-                location = pathlib.Path(root, name)
-                test_item.files.append(
-                    File(path=str(location.relative_to(here)), content=location.read_text())
-                )
-    if "myapp2" in installed_apps:
-        for root, _, files in os.walk(here / "myapp2"):
-            for name in files:
-                location = pathlib.Path(root, name)
-                test_item.files.append(
-                    File(path=str(location.relative_to(here)), content=location.read_text())
-                )
+        current_settings: pathlib.Path = scenario.path_for("mysettings.py")
+
+        if custom_settings is None and installed_apps is None and monkeypatch is None:
+            if current_settings.exists():
+                return options
+
+        if not current_settings.exists():
+            with open(current_settings, "w") as fle:
+                fle.write("")
+
+        original_settings_text = current_settings.read_text()
+
+        if monkeypatch is not None:
+            monkeypatch_str = "import django_stubs_ext\ndjango_stubs_ext.monkeypatch()\n"
+            settings_text = original_settings_text.replace(monkeypatch_str, "")
+            if monkeypatch:
+                settings_text = monkeypatch_str + settings_text
+
+            with open(current_settings, "w") as fle:
+                fle.write(settings_text)
+
+        settings_values = runpy.run_path(str(current_settings))
+
+        if "INSTALLED_APPS" not in settings_values:
+            with open(current_settings, "a") as fle:
+                fle.write("\nINSTALLED_APPS = None")
+
+        if "SECRET_KEY" not in settings_values:
+            with open(current_settings, "a") as fle:
+                fle.write('\nSECRET_KEY = "1"')
+
+        settings = ast.parse(current_settings.read_text())
+
+        class Fixer(ast.NodeTransformer):
+            def visit_Assign(self, node: ast.Assign) -> ast.Assign:
+                match node.targets:
+                    case [ast.Name(id="INSTALLED_APPS")]:
+                        nonlocal installed_apps
+                        if not isinstance(node.value, ast.List):
+                            installed_apps = installed_apps or ["myapp", "myapp2"]
+                        elif installed_apps is None and isinstance(
+                            settings_values["INSTALLED_APPS"], list
+                        ):
+                            installed_apps = settings_values["INSTALLED_APPS"]
+
+                        if not isinstance(installed_apps, list):
+                            installed_apps = []
+
+                        if "django.contrib.contenttypes" not in installed_apps:
+                            installed_apps.insert(0, "django.contrib.contenttypes")
+
+                        return ast.Assign(
+                            targets=node.targets,
+                            value=ast.List(
+                                elts=[ast.Constant(value=app) for app in installed_apps]
+                            ),
+                        )
+                    case _:
+                        return node
+
+        Fixer().visit(settings)
+        with open(current_settings, "w") as fle:
+            fle.write(ast.unparse(ast.fix_missing_locations(settings)))
+            if isinstance(custom_settings, str):
+                fle.write("\n")
+                fle.write(custom_settings)
+
+        if current_settings.read_text() != original_settings_text:
+            new_settings = current_settings.read_text()
+            current_settings.write_text("")
+            # Make it recorded that the settings changed
+            scenario.handle_followup_file(
+                FollowupFile(path=current_settings.name, content=new_settings)
+            )
+
+        settings_values = runpy.run_path(str(current_settings))
+        installed_apps = settings_values["INSTALLED_APPS"]
+
+        if not isinstance(installed_apps, list):
+            installed_apps = []
+
+        if "myapp" in installed_apps:
+            for root, _, files in os.walk(here / "myapp"):
+                for name in files:
+                    location = pathlib.Path(root, name)
+                    scenario.handle_followup_file(
+                        FollowupFile(
+                            path=str(location.relative_to(here)), content=location.read_text()
+                        )
+                    )
+        if "myapp2" in installed_apps:
+            for root, _, files in os.walk(here / "myapp2"):
+                for name in files:
+                    location = pathlib.Path(root, name)
+                    scenario.handle_followup_file(
+                        FollowupFile(
+                            path=str(location.relative_to(here)), content=location.read_text()
+                        )
+                    )
+
+        return options
+
+
+if TYPE_CHECKING:
+    _eh: ExtensionHook = django_plugin_hook
+    _sh: ScenarioHookMaker = Hooks
